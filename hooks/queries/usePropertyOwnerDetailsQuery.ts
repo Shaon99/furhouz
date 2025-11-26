a70ecc85bd08555ae06042939074792feece6e31
mujahidjuel3 <mujahidjuel056@gmail.com>
2025-11-26 14:41:14 +0600

    Done.

--- hooks/queries/usePropertyOwnerDetailsQuery.ts
+++ hooks/queries/usePropertyOwnerDetailsQuery.ts
@@ -14,7 +14,7 @@ export function usePropertyOwnerDetailsQuery() {
             
             return data.data;
         },
-        staleTime: 1000 * 60 * 5, // 5 minutes
+        staleTime: 1000 * 60 * 5, 
         refetchOnWindowFocus: false,
         refetchOnMount: true,
         refetchOnReconnect: false,
